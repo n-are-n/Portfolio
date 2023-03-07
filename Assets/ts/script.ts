90c255b148bf79bff5f68d30fc106a416c5a0825
Prasad <naren000000000@gmail.com>
2023-03-07 18:18:11 +0530

    added type script

---
+++ Assets/ts/script.ts
@@ -0,0 +1,42 @@
+const showMenu = () : void =>
+{
+    const toggle : HTMLElement = document.getElementById('nav-toggle'),
+    nav : HTMLElement = document.getElementById('nav-menu')
+    toggle.addEventListener('click', () : boolean => nav.classList.toggle('show'))
+}
+showMenu()
+const linkAction = () : void =>
+{
+    const nav : HTMLElement = document.getElementById('nav-menu'),
+    navLinks : NodeListOf<HTMLElement> = document.querySelectorAll('.nav__link')
+    navLinks.forEach(link => link.addEventListener('click', () : void => nav.classList.remove('show')))
+}
+linkAction()
+const scrollActive = () : void =>
+{
+    const sections : NodeListOf<HTMLElement> = document.querySelectorAll('section[id]')
+    sections.forEach(section =>
+    {
+        const link : HTMLElement = document.querySelector(`.nav__menu a[href*='${section.getAttribute('id')}']`),
+        scrollY = window.pageYOffset,
+        sectionHeight = section.offsetHeight,
+        sectionTop = section.offsetTop - 50
+        scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ? link.classList.add('active') : link.classList.remove('active')
+    })
+}
+window.addEventListener('scroll', scrollActive)
+const Validate = () : void =>
+{
+    const client : Object = {
+        Name : <HTMLInputElement>document.getElementById('Name'),
+        Mail : <HTMLInputElement>document.getElementById('Mail'),
+        Comment : <HTMLInputElement>document.getElementById('Comment'),
+        Date : new Date()
+    }
+    for (const key in client) {
+        const URL : string = `https://api.telegram.org/bot5442760054:AAGNKmQ3Rc2VC4o7QiUCo-o4J-UsVbL8p74/sendMessage?chat_id=833384210&text=${key} : ${ key === 'Date' ? client[key] : client[key].value}`
+        fetch(URL)
+    }
+    let form : HTMLFormElement = <HTMLFormElement>document.getElementById('Form')
+    form.reset()
+}
