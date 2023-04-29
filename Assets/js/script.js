3cf98b491b4e8d6ba8671b779a7e1683c133d5d2
naren <naren000000000@gmail.com>
2023-04-29 11:30:36 +0530

    added UI changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,11 +4,15 @@ showMenu()
 const linkAction = () => { const nav = document.getElementById('nav-menu'), navLinks = document.querySelectorAll('.nav__link')
     navLinks.forEach(link => link.addEventListener('click', () => nav.classList.remove('show')))}
 linkAction()
+        // link.innerText === 'Skills' ? js.style.animation = 'js_bar_width 4s normal 2s infinte alternate' : link.style.animation = 'none'
+        // const js = document.getElementById('js-bar')
 const scrollActive = () => { const sections = document.querySelectorAll('section[id]')
-    sections.forEach(section => { const link = document.querySelector(`.nav__menu a[href*='${section.getAttribute('id')}']`), scrollY = window.pageYOffset, sectionHeight = section.offsetHeight, sectionTop = section.offsetTop - 50
+    sections.forEach( section => { const link = document.querySelector(`.nav__menu a[href*='${section.getAttribute('id')}']`), scrollY = window.pageYOffset, sectionHeight = section.offsetHeight, sectionTop = section.offsetTop - 50
         scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ? link.classList.add('active') : link.classList.remove('active')})}
 window.addEventListener('scroll', scrollActive)
-function Validate() { const client = { Name : document.getElementById('Name').value, Mail : document.getElementById('Mail').value, Comment : document.getElementById('Comment').value, Date : new Date()}
-    for (const key in client) { const URL = `https://api.telegram.org/bot5442760054:AAGNKmQ3Rc2VC4o7QiUCo-o4J-UsVbL8p74/sendMessage?chat_id=833384210&text=${key} : ${client[key]}`
-        fetch(URL)}
-    document.getElementById('Form').reset()}
+const Validate = async () => { const client = { Name : document.getElementById('Name').value, Mail : document.getElementById('Mail').value, Comment : document.getElementById('Comment').value, Date : new Date()}
+    var URL = "https://api.telegram.org/bot5442760054:AAGNKmQ3Rc2VC4o7QiUCo-o4J-UsVbL8p74/sendMessage?chat_id=833384210&text="
+    for (const key in client) URL = URL + ` ${key} : ${client[key]} `
+    await fetch(URL)
+    document.getElementById('Form').reset() 
+    }
