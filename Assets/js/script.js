76049cb8222c20fdfb011397c52d6c73df3cebee
naren <naren000000000@gmail.com>
2023-09-19 23:07:52 +0530

    added profile urls

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,11 +7,11 @@ linkAction()
         // link.innerText === 'Skills' ? js.style.animation = 'js_bar_width 4s normal 2s infinte alternate' : link.style.animation = 'none'
         // const js = document.getElementById('js-bar')
 const scrollActive = () => { const sections = document.querySelectorAll('section[id]')
-    sections.forEach( section => { const link = document.querySelector(`.nav__menu a[href*='${section.getAttribute('id')}']`), scrollY = window.pageYOffset, sectionHeight = section.offsetHeight, sectionTop = section.offsetTop - 50
+    sections.forEach( section => { const link = document.querySelector(`.nav__menu a[href*='${section.getAttribute('id')}']`), scrollY = window.scrollY, sectionHeight = section.offsetHeight, sectionTop = section.offsetTop - 50
         scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ? link.classList.add('active') : link.classList.remove('active')})}
 window.addEventListener('scroll', scrollActive)
 const Validate = async () => { const client = { Name : document.getElementById('Name').value, Mail : document.getElementById('Mail').value, Comment : document.getElementById('Comment').value, Date : new Date()}
-    var URL = 'https://api.telegram.org/bot5442760054:AAGNKmQ3Rc2VC4o7QiUCo-o4J-UsVbL8p74/sendMessage?chat_id=833384210&text='
+    let URL = 'https://api.telegram.org/bot5442760054:AAGNKmQ3Rc2VC4o7QiUCo-o4J-UsVbL8p74/sendMessage?chat_id=833384210&text='
     for (const key in client) URL = URL + ` ${key} : ${client[key]} `
     await fetch(URL)
     document.getElementById('Form').reset() 
--- Assets/ts/script.ts
+++ Assets/ts/script.ts
@@ -5,10 +5,10 @@ const linkAction = () : void => { const nav : HTMLElement = document.getElementB
     navLinks.forEach(link => link.addEventListener('click', () : void => nav.classList.remove('show')))}
 linkAction()
 const scrollActive = () : void => { const sections : NodeListOf<HTMLElement> = document.querySelectorAll('section[id]')
-    sections.forEach(section  => { const link : HTMLElement = document.querySelector(`.nav__menu a[href*='${section.getAttribute('id')}']`), scrollY :number  = window.pageYOffset, sectionHeight : number = section.offsetHeight, sectionTop : number = section.offsetTop - 50
-        scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ? <void>link.classList.add('active') : <void>link.classList.remove('active')})}
+    sections.forEach(section  => { const link : HTMLElement = document.querySelector(`.nav__menu a[href*='${section.getAttribute('id')}']`), scrollY :number  = window.scrollY, sectionHeight : number = section.offsetHeight, sectionTop : number = section.offsetTop - 50
+        scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ? link.classList.add('active') : link.classList.remove('active')})}
 window.addEventListener('scroll', scrollActive)
-const Validate = () : void => { const client : Object = { Name : <HTMLInputElement>document.getElementById('Name'), Mail : <HTMLInputElement>document.getElementById('Mail'), Comment : <HTMLInputElement>document.getElementById('Comment'), Date : <Date>new Date()}
+const Validate = () : void => { const client : Object = { Name : <HTMLInputElement>document.getElementById('Name'), Mail : <HTMLInputElement>document.getElementById('Mail'), Comment : <HTMLInputElement>document.getElementById('Comment'), Date : new Date()}
     let URL : string = 'https://api.telegram.org/bot5442760054:AAGNKmQ3Rc2VC4o7QiUCo-o4J-UsVbL8p74/sendMessage?chat_id=833384210&text='
     for (const key in client) URL = URL + ` ${key} : ${ key === 'Date' ? client[key] : client[key].value} `
     fetch(URL)
